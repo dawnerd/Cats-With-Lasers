1d4ac2d79273f098dce78c47bbdf740f4daa4468
Troy Whiteley <dawnerd@gmail.com>
2012-10-27 23:53:29 -0700

    First commit
    Added base scripts including jquery.

---
+++ js/core.js
@@ -0,0 +1,40 @@
+﻿/**
+ * Catswithus
+ *
+ * Copyright 2012 Troy Whiteley <troy@somanyscientists.com>
+ *
+ * http://catswith.us
+*/
+
+// Clears up junk output in Visual Studio
+console.clear && console.clear()
+
+var Cats = {};
+
+(function () {
+  "use strict";
+
+  Cats.environment = 'web';
+
+  var Game = function () {
+    var i;
+
+    this.numberOfItems = 1;
+    this.itemSpeed = 1;
+    this.itemsClicked = 0;
+    this.items = [];
+    this.itemTemplate = {
+      color: '#fff',
+      currentPos: [0, 0],
+      targetPos: [100, 100],
+      finalPos: [500,0]
+    };
+    this.container = $('#Playfield');
+
+    //generate items
+    for (i = 0; i < this.numberOfItems; i++) {
+
+    }
+  }
+  Cats.Game = Game;
+})();
---
+++ js/currentEnvironment.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Catswithus
+ *
+ * Copyright 2012 Troy Whiteley <troy@somanyscientists.com>
+ *
+ * http://catswith.us
+*/
+
+(function () {
+  "use strict";
+
+  if (typeof WinJS === 'object') {
+    Cats.environment = 'windows';
+  }
+})();
